2d705e8b59203ab6e75071e023abb4eb0aa2ea55
Pritam <pritamsamaddar840@gmail.com>
2025-05-10 22:22:56 +0530

    done

--- src/app/components/HalfCircleDonut.tsx
+++ src/app/components/HalfCircleDonut.tsx
@@ -33,7 +33,6 @@ export function HalfCircleDonut({ data }: DonutProps) {
       <div className='absolute mt-[42px]'>
         <p className="text-[12px] font-bold text-center text-[#7D7D7E]">Total</p>
         <p className="text-[18px] font-bold text-center">{topCitiesData.total}</p>
-
         <div className='flex flex-row items-center'>
           <ArrowUp color="#1D874F" className="w-[14px] h-[14px]" />
           <p className="text-xs text-green-600 text-center">{topCitiesData.change}</p>
